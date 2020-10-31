aef44ceee615fadef0602a33fee239f76cdf1c93
Chris Chuang <chrischuang0421@gmail.com>
2020-10-31 18:36:58 +0800

    Cache Management - Refine query config code

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {ReactQueryConfigProvider} from 'react-query';
+import {ReactQueryConfigProvider, ReactQueryConfig} from 'react-query';
 
 import {loadDevTools} from './dev-tools/load';
 import './bootstrap';
 import {App} from './app';
 import {CustomError} from './test/types';
 
+const queryConfig: ReactQueryConfig = {
+  queries: {
+    useErrorBoundary: true,
+    refetchOnWindowFocus: false, // window 進入 focus 時不要重新 qurey
+    retry(failureCount, error) {
+      if ((error as CustomError).status === 404) {
+        console.log(`404 ERROR, STOP retry`);
+        return false;
+      }
+      if (failureCount >= 2) {
+        return false;
+      }
+      return true;
+    },
+  },
+};
+
 loadDevTools(() => {
   ReactDOM.render(
-    <ReactQueryConfigProvider
-      config={{
-        queries: {
-          useErrorBoundary: true,
-          refetchOnWindowFocus: false, // window 進入 focus 時不要重新 qurey
-          retry(failureCount, error) {
-            if ((error as CustomError).status === 404) {
-              console.log(`404 ERROR, STOP retry`);
-              return false;
-            }
-            if (failureCount >= 2) {
-              return false;
-            }
-            return true;
-          },
-        },
-      }}
-    >
+    <ReactQueryConfigProvider config={queryConfig}>
       <App />
     </ReactQueryConfigProvider>,
     document.getElementById('root'),
