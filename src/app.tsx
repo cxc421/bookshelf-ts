1e143c7b3c6d0d90e9a79f4cbf4211657da77dea
Chris Chuang <chrischuang0421@gmail.com>
2020-11-01 14:25:07 +0800

    Context - colocate global providers to single AppProviders

--- src/app.tsx
+++ src/app.tsx
@@ -1,21 +1,13 @@
 /** @jsx jsx */
 import {jsx} from '@emotion/core';
 
-import {BrowserRouter as Router} from 'react-router-dom';
 import {useAuth} from 'context/auth-context';
 import {AuthenticatedApp} from 'authenticated-app';
 import {UnauthenticatedApp} from 'unauthenticated-app';
 
 function App() {
   const {user} = useAuth();
-  if (!user) {
-    return <UnauthenticatedApp />;
-  }
-  return (
-    <Router>
-      <AuthenticatedApp />
-    </Router>
-  );
+  return !user ? <UnauthenticatedApp /> : <AuthenticatedApp />;
 }
 
 export {App};
--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {ReactQueryConfigProvider, ReactQueryConfig} from 'react-query';
 
 import {loadDevTools} from './dev-tools/load';
 import './bootstrap';
 import {App} from './app';
-import {AuthProvider} from 'context/auth-context';
-import {CustomError} from './test/types';
-
-const queryConfig: ReactQueryConfig = {
-  queries: {
-    useErrorBoundary: true,
-    refetchOnWindowFocus: false, // window 進入 focus 時不要重新 qurey
-    retry(failureCount, error) {
-      if ((error as CustomError).status === 404) {
-        console.log(`404 ERROR, STOP retry`);
-        return false;
-      }
-      if (failureCount >= 2) {
-        return false;
-      }
-      return true;
-    },
-  },
-};
+import {AppProviders} from 'context';
 
 loadDevTools(() => {
   ReactDOM.render(
-    <ReactQueryConfigProvider config={queryConfig}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </ReactQueryConfigProvider>,
+    <AppProviders>
+      <App />
+    </AppProviders>,
     document.getElementById('root'),
   );
 });
