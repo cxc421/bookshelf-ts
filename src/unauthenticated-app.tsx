4bed7c13dd25d8a77817f795adb3f75b1fe4540f
Chris Chuang <chrischuang0421@gmail.com>
2020-11-01 21:52:24 +0800

    Compound Components - Reset modal to empty file

--- src/unauthenticated-app.tsx
+++ src/unauthenticated-app.tsx
@@ -2,12 +2,54 @@
 import {jsx} from '@emotion/core';
 
 import 'bootstrap/dist/css/bootstrap-reboot.css';
+import React, {FC, ReactElement} from 'react';
+import VisuallyHidden from '@reach/visually-hidden';
 import '@reach/dialog/styles.css';
-import {Button} from './components/lib';
-import {Modal, ModalContents, ModalOpenButton} from './components/modal';
+import {Button, Dialog, CircleButton} from './components/lib';
+// import {Modal, ModalContents, ModalOpenButton} from './components/modal';
 import {Logo} from './components/Logo';
 import {LoginForm} from './components/LoginForm';
 import {useAuth} from 'context/auth-context';
+
+// 💣 when you're all done, you'll be able to completely delete this
+type LoginFormModalProps = {
+  onSubmit: (formData: {username: string; password: string}) => Promise<void>;
+  modalTitleText: string;
+  modalLabelText: string;
+  submitButton: ReactElement;
+  openButton: ReactElement;
+};
+
+const LoginFormModal: FC<LoginFormModalProps> = ({
+  onSubmit,
+  modalTitleText,
+  modalLabelText,
+  submitButton,
+  openButton,
+}) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  return (
+    <React.Fragment>
+      {React.cloneElement(openButton, {onClick: () => setIsOpen(true)})}
+      <Dialog
+        aria-label={modalLabelText}
+        isOpen={isOpen}
+        onDismiss={() => setIsOpen(false)}
+      >
+        <div css={{display: 'flex', justifyContent: 'flex-end'}}>
+          {/* 💰 here's what you should put in your <ModalDismissButton> */}
+          <CircleButton onClick={() => setIsOpen(false)}>
+            <VisuallyHidden>Close</VisuallyHidden>
+            <span aria-hidden>×</span>
+          </CircleButton>
+        </div>
+        <h3 css={{textAlign: 'center', fontSize: '2em'}}>{modalTitleText}</h3>
+        <LoginForm onSubmit={onSubmit} submitButton={submitButton} />
+      </Dialog>
+    </React.Fragment>
+  );
+};
 
 const UnauthenticatedApp = () => {
   const {login, register} = useAuth();
@@ -31,28 +73,26 @@ const UnauthenticatedApp = () => {
           gridGap: '0.75rem',
         }}
       >
-        <Modal>
-          <ModalOpenButton>
-            <Button variant="primary">Login</Button>
-          </ModalOpenButton>
-          <ModalContents aria-label="Login Form" title="Login">
-            <LoginForm
-              onSubmit={login}
-              submitButton={<Button variant="primary">Login</Button>}
-            />
-          </ModalContents>
-        </Modal>
-        <Modal>
-          <ModalOpenButton>
-            <Button variant="secondary">Register</Button>
-          </ModalOpenButton>
-          <ModalContents aria-label="Registration form" title="Register">
-            <LoginForm
-              onSubmit={register}
-              submitButton={<Button variant="secondary">Register</Button>}
-            />
-          </ModalContents>
-        </Modal>
+        {/* 🐨 replace both of these with the Modal compound components */}
+        {/*
+          🦉 when you're done, it'll look a lot more complicated than
+             it did when you started, but the extra credits will help clean
+             things up a bit.
+        */}
+        <LoginFormModal
+          onSubmit={login}
+          modalTitleText="Login"
+          modalLabelText="Login form"
+          submitButton={<Button variant="primary">Login</Button>}
+          openButton={<Button variant="primary">Login</Button>}
+        />
+        <LoginFormModal
+          onSubmit={register}
+          modalTitleText="Register"
+          modalLabelText="Registration form"
+          submitButton={<Button variant="secondary">Register</Button>}
+          openButton={<Button variant="secondary">Register</Button>}
+        />
       </div>
     </div>
   );
